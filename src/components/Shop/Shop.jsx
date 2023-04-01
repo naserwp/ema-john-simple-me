e11621f50f996ce3710e153a2a4c0ded5a801abe
naserwp <naserwp@gmail.com>
2023-04-02 04:32:19 +0600

    äddedProduct making

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,20 +1,38 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import '../Shop/Shop.css'
 const Shop = () => {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([])
-
     useEffect(() => {
         fetch('products.json')
             .then(res => res.json())
             .then(data => setProducts(data))
-
     }, [])
+
+    useEffect( () => {
+        console.log('products', products)
+        const storedCart = getShoppingCart();
+        //Step 1 to get ID
+
+        for(const id in storedCart){
+            //step 2 get product by id 
+            const addedProduct = products.find(product => product.id === id);
+            //step 3 get quantity products
+            const quantity = storedCart[id];
+            addedProduct.quantity = quantity;
+            console.log('saveProduct', addedProduct)
+        }
+
+    } , [])
+
+
     const handleAddToCart = (product) => {
         const newCart = [...cart, product];
         setCart(newCart)
+        addToDb(product.id)
     }
 
     return (
